7ba52b2ef5c3845ed62dce6b777100060ec253fd
Simon Skrødal <simon.skrodal@gmail.com>
2015-09-16 17:02:54 +0930

    Tidy

--- lib/relay.db.class.php
+++ lib/relay.db.class.php
@@ -1,81 +1,89 @@
 <?php
-/**
- * Handles DB Connection and queries
- *
- * @author Simon Skrodal
- * @since  August 2015
- */
-
-
-class RelayDB {
-	private $conn, $config; 
-	private $DEBUG = true;
-
-	function __construct($config) {
-		$this->config = $config;
-	}
-
-
 	/**
-	 * 
+	 * Handles DB Connection and queries
+	 *
+	 * @author Simon Skrodal
+	 * @since  August 2015
 	 */
-	public function query($sql){
-		// 
-		$this->connect();
-		// Run query
-		$query = mssql_query($sql, $this->conn);
-		// On error
-		if($query === FALSE){ Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB query failed.'); }
-		// Response
-		$response = [];
-		//
-		$this->_logger("Rows returned: " . mssql_num_rows($query), __LINE__, __FUNCTION__);
-		// Loop rows and add to response array
-		if (mssql_num_rows($query) > 0) {
-		    while ($row = mssql_fetch_assoc($query)) {
-		        $response[] = $row;
-			    $this->_logger(print_r($response, true), __LINE__, __FUNCTION__);
-		    }
+
+	ini_set('mssql.charset', 'UTF-8');
+
+	class RelayDB {
+		private $conn, $config;
+		private $DEBUG = true;
+
+		function __construct($config) {
+			$this->config = $config;
 		}
-		// Free the query result
-		mssql_free_result($query);
-		// Close link
-		$this->close();
-		//
-		return $response;
-	}
 
 
-	/**
-	 *	Open MSSQL connection
-	 */
-	private function connect(){
-		// 
-		$this->conn = mssql_connect($this->config['host'], $this->config['user'], $this->config['pass']);
-		// 
-		if(!$this->conn){ Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB connection failed.'); }
-		//
-		if(!mssql_select_db($this->config['db'])) { Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB table connection failed.'); }
+		/**
+		 *
+		 */
+		public function query($sql) {
+			//
+			$this->connect();
+			// Run query
+			$query = mssql_query($sql, $this->conn);
+			// On error
+			if($query === false) {
+				Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB query failed.');
+			}
+			// Response
+			$response = [];
+			//
+			$this->_logger("Rows returned: " . mssql_num_rows($query), __LINE__, __FUNCTION__);
+			// Loop rows and add to response array
+			if(mssql_num_rows($query) > 0) {
+				while($row = mssql_fetch_assoc($query)) {
+					$response[] = $row;
+					$this->_logger(print_r($response, true), __LINE__, __FUNCTION__);
+				}
+			}
+			// Free the query result
+			mssql_free_result($query);
+			// Close link
+			$this->close();
 
-		$this->_logger("DB CONNECTED", __LINE__, __FUNCTION__);
-	}
-
-	/**
-	 *	Close MSSQL connection
-	 */
-	private function close(){
-		if($this->conn !== FALSE) {
-			mssql_close($this->conn);	
+			//
+			return $response;
 		}
-		$this->_logger("DB CLOSED", __LINE__, __FUNCTION__);
-	}
 
-	/**
-	 * DEV
-	 */
-	private function _logger($text, $line, $function) {
-		if($this->DEBUG) {
-			error_log($function . '(' . $line . '): ' . $text);
+
+		/**
+		 *    Open MSSQL connection
+		 */
+		private function connect() {
+			//
+			$this->conn = mssql_connect($this->config['host'], $this->config['user'], $this->config['pass']);
+			//
+			if(!$this->conn) {
+				Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB connection failed.');
+			}
+			//
+			if(!mssql_select_db($this->config['db'])) {
+				Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB table connection failed.');
+			}
+
+			$this->_logger("DB CONNECTED", __LINE__, __FUNCTION__);
+		}
+
+		/**
+		 *    Close MSSQL connection
+		 */
+		private function close() {
+			if($this->conn !== false) {
+				mssql_close($this->conn);
+			}
+			$this->_logger("DB CLOSED", __LINE__, __FUNCTION__);
+		}
+
+		/**
+		 * DEV
+		 */
+		private function _logger($text, $line, $function) {
+			if($this->DEBUG) {
+				error_log($function . '(' . $line . '): ' . $text);
+			}
 		}
 	}
-}

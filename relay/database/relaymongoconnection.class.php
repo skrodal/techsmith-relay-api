82655bec6303071a51bacb7df9968d0c53709be7
Simon Skrødal <simon.skrodal@gmail.com>
2015-10-30 15:05:22 +1030

    Mongo Auth

--- relay/database/relaymongoconnection.class.php
+++ relay/database/relaymongoconnection.class.php
@@ -52,7 +52,7 @@
 
 		private function getConnection(){
 			try {
-				return new MongoClient("mongodb://" . $this->config['user'] . ":" . $this->config['pass'] . "@127.0.0.1");
+				return new MongoClient("mongodb://" . $this->config['user'] . ":" . $this->config['pass'] . "@127.0.0.1/" . $this->config['db']);
 			} catch (MongoConnectionException $e){
 				Utils::log($e->getMessage());
 				Response::error(500, $_SERVER["SERVER_PROTOCOL"] . ' DB connection failed (MongoDB).');

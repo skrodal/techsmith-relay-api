096e289e1361157ee4cf6bc672413118651147a6
Simon Skrødal <simon.skrodal@gmail.com>
2016-07-04 16:02:48 +0930

    Debug

--- relay/database/relaymysqlconnection.class.php
+++ relay/database/relaymysqlconnection.class.php
@@ -57,7 +57,7 @@
 			$mysqli = new \mysqli($this->config['db_host'], $this->config['db_user'], $this->config['db_pass'], $this->config['db_name']);
 			//
 			if($mysqli->connect_errno) {
-				Response::error(503, "503 Service Unavailable (DB connection failed (MySQL): " . $this->config['db_name']);
+				Response::error(503, "503 Service Unavailable (DB connection failed (MySQL): " . $mysqli->error . "(" . $this->config['db_name'] . ")");
 			}
 
 			Utils::log("MySQL DB CONNECTED");

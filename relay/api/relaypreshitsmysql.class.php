2b864ef592478d89d48fe82e82cc9d17d08be7cb
Simon Skrødal <simon.skrodal@gmail.com>
2016-09-26 17:04:00 +0930

    Route update

--- relay/api/relaypreshitsmysql.class.php
+++ relay/api/relaypreshitsmysql.class.php
@@ -78,7 +78,7 @@
 			foreach($orgs as $index => $org){
 				$result = $this->sql->query("SELECT SUM(hits) FROM $this->tableHits AS hits WHERE path LIKE '%$org%'");
 				$hits = $result->fetch_assoc();
-				$response[$org] = $hits['hits'] ? $hits['hits'] : 0;
+				$response[$org] = $hits ? $hits : 0;
 			}
 			return $response;
 		}

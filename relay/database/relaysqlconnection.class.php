84af453857442ee3ea13b993d7e0877b0ff283b6
Simon Skrødal <simon.skrodal@gmail.com>
2016-09-29 13:00:49 +0930

    typo

--- relay/database/relaysqlconnection.class.php
+++ relay/database/relaysqlconnection.class.php
@@ -63,7 +63,7 @@
 			$pass = $this->config['pass'];
 			try {
 				//$connection = new \PDO("mssql:host=$host;dbname=$db;charset=UTF8", $user, $pass);
-				$connection = new PDO("sqlsrv:Server=$host;Database=$db", $user, $pass);
+				$connection = new \PDO("sqlsrv:Server=$host;Database=$db", $user, $pass);
 				$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				Utils::log("DB CONNECTED");
 				return $connection;
